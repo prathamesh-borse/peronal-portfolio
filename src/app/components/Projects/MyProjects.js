f9201d222759cc406385073c3736dba964286750
Prathamesh Borse <prathameshborse.official@gmail.com>
2025-08-09 20:25:56 +0530

    Improved Projects Section

--- src/app/components/Projects/MyProjects.js
+++ src/app/components/Projects/MyProjects.js
@@ -39,6 +39,86 @@ const MyProjects = () => {
                       <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>{" "}
                     </svg>
                   </div>
+                  <div className="flex items-center space-x-1 ">
+                    <a
+                      href="https://prathameshportfolio.onrender.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cursor-pointer text-white p-1 rounded-full transition-all duration-300 hover:opacity-70 mb-1"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        className="w-12 h-12 fill-[#00C2CB]"
+                      >
+                        <path d="M14 3h7v7h-2V5.41l-9.29 9.3l-1.42-1.42l9.3-9.29H14V3M5 5h7v2H5v12h12v-7h2v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2Z" />
+                      </svg>
+                    </a>
+                  </div>
+                  <div className="flex items-center">
+                    <a
+                      href="https://github.com/prathamesh-borse/peronal-portfolio"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className=" cursor-pointer text-white p-1 rounded-full transition-all duration-300 hover:opacity-70"
+                    >
+                      <Image
+                        src="/assets/github-icon.svg"
+                        alt="Github Icon"
+                        width={50}
+                        height={30}
+                        className="filter-sky transition-all duration-300 hover:filter-none"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <span className="text-white text-xl font-semibold mb-8">
+                  Personal Portfolio Website
+                </span>
+                <span className="text-white text-base font-medium mb-8">
+                  Developed a Personal Portfolio Website using React, Talwind
+                  CSS, Framer Motion, and JavaScript. The website showcases my
+                  skills, projects, and experiences, providing a platform for
+                  potential employers or clients to view my work and contact me.
+                  It features a responsive design, ensuring compatibility across
+                  various devices and screen sizes.
+                </span>
+
+                <div className="opacity-40 font-medium flex tex-white mt-8">
+                  <span className="mr-5">React</span>
+                  <span className="mr-5">Talwind CSS</span>
+                  <span className="mr-5">JavaScript</span>
+                  <span className="mr-5">Partical.js</span>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, scale: 0.4 }}
+            animate={isInView ? { opacity: 1, scale: 1 } : {}}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+          >
+            <div className="hover:-translate-y-1 hover:bg-pink-600 hover:opacity-100 transition-all duration-300  bg-[rgb(43,43,43)] rounded-lg p-5 flex items-center justify-center">
+              <div className="flex flex-col text-left">
+                <div className="flex flex-row items-center justify-between mb-5">
+                  <div id="logos" className="flex items-center justify-between">
+                    <svg
+                      width="40"
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="#00c2cb"
+                      strokeWidth="1"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      {" "}
+                      <title>Folder</title>{" "}
+                      <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>{" "}
+                    </svg>
+                  </div>
                   <div className="flex items-center">
                     <a
                       href="https://github.com/prathamesh-borse/Library-Management-System"
@@ -101,7 +181,7 @@ const MyProjects = () => {
                   </div>
                   <div className="flex items-center space-x-1 ">
                     <a
-                      href="https://prathamesh-borse.github.io/Bootstrap_4_Responsive_Website/"
+                      href="https://googlegeminigpt.vercel.app/"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="cursor-pointer text-white p-1 rounded-full transition-all duration-300 hover:opacity-70 mb-1"
@@ -114,36 +194,25 @@ const MyProjects = () => {
                         <path d="M14 3h7v7h-2V5.41l-9.29 9.3l-1.42-1.42l9.3-9.29H14V3M5 5h7v2H5v12h12v-7h2v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2Z" />
                       </svg>
                     </a>
-                    <a
-                      href="https://github.com/prathamesh-borse/Bootstrap_4_Responsive_Website"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="cursor-pointer text-white p-1 rounded-full transition-all duration-300 hover:opacity-70"
-                    >
-                      <Image
-                        src="/assets/github-icon.svg"
-                        alt="Github Icon"
-                        width={50}
-                        height={30}
-                        className="filter-sky transition-all duration-300 hover:filter-none"
-                      />
-                    </a>
                   </div>
                 </div>
                 <span className="text-white text-xl font-semibold mb-8">
-                  Responsive Travel Website
+                  Gemini Chatbot
                 </span>
                 <span className="text-white text-base font-medium mb-8">
-                  Developed a Responsive Travel Website using HTML, CSS,
-                  JavaScript and Bootstrap. Used Media Queries for
-                  responsiveness for the website and added beautiful animations
-                  in the website which enhances the website look.
+                  Developed a Gemini Chatbot using HTML, CSS, and Bootstrap 4.
+                  The chatbot is designed to provide users with an interactive
+                  experience, allowing them to ask questions and receive
+                  responses in real-time. The project showcases the use of
+                  responsive design principles to ensure compatibility across
+                  various devices.
                 </span>
 
                 <div className="opacity-40 font-medium flex tex-white mt-8">
-                  <span className="mr-5">HTML</span>
+                  <span className="mr-5">React</span>
                   <span className="mr-5">CSS</span>
                   <span className="mr-5">Bootstrap</span>
+                  <span className="mr-5">JavaScript</span>
                 </div>
               </div>
             </div>
